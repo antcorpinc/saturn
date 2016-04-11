401cca9647a428c99c07eb3fcefbad6a359faf12
antcorpinc <antcorpinc@gmail.com>
2016-04-11 10:04:03 +0530

    Update from VS

--- src/Saturn/Startup.cs
+++ src/Saturn/Startup.cs
@@ -17,7 +17,6 @@ namespace Saturn
         {
 
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
