9564b56a6cfc9a6658a99288ddb99f44182f4b9b
antcorpinc <antcorpinc@gmail.com>
2016-04-27 13:29:46 +0530

    Implement basic security -register/login/logoff

--- src/Saturn/Controllers/Mvc/AccountController.cs
+++ src/Saturn/Controllers/Mvc/AccountController.cs
@@ -25,10 +25,8 @@ namespace Saturn.Controllers.Mvc
         public IActionResult Index()
         {
             return View();
-        }
-
-    
-        // GET: /Account/Register
+        }    
+       
         [HttpGet]
         [AllowAnonymous]
         public IActionResult Register()
@@ -52,6 +50,60 @@ namespace Saturn.Controllers.Mvc
                 }
             }
             return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(AccountLoginViewModel model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                // This doesn't count login failures towards account lockout
+                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                var result = await _securityAdapter.LoginAsync(model.LoginEmail, model.Password, model.RememberMe);
+                if (result)
+                {
+                     return RedirectToLocal(returnUrl);
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return View(model);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public  IActionResult LogOff()
+        {
+             _securityAdapter.LogoutAsync();           
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
         }
     }
 }
---
+++ src/Saturn/ViewModels/AccountLoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Saturn.ViewModels
+{
+    public class AccountLoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string LoginEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
