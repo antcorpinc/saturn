8820b396b0ecf8a1a9dfe9dded28a0802a0db88c
antcorpinc <antcorpinc@gmail.com>
2016-05-13 11:56:47 +0530

    bootstrapping main app component

--- src/Saturn/wwwroot/app/app.component.js
+++ src/Saturn/wwwroot/app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['@angular/core', '@angular/http'], function(exports_1, context_1) {
+System.register(['@angular/core'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,24 +10,19 @@ System.register(['@angular/core', '@angular/http'], function(exports_1, context_
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, http_1;
+    var core_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
-            },
-            function (http_1_1) {
-                http_1 = http_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
                 }
                 AppComponent = __decorate([
-                    core_1.Component({
-                        providers: [http_1.HTTP_PROVIDERS]
-                    }), 
+                    core_1.Component({}), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
--- src/Saturn/wwwroot/app/app.component.ts
+++ src/Saturn/wwwroot/app/app.component.ts
@@ -1,8 +1,6 @@
 import {Component} from '@angular/core';
-import {HTTP_PROVIDERS} from '@angular/http';
-
 @Component({
-    providers:[HTTP_PROVIDERS]
+    
 })
 
 export class AppComponent{
